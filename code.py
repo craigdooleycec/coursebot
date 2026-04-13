4dd041ce13a62fadf42d274319550063f3b90585
craigdooleycec <craigdooley@yahoo.com>
2026-04-13 14:51:03 +0100

    Add files via upload

--- code.py
+++ code.py
@@ -13,7 +13,7 @@ def chat():
     user_message = request.json["message"]
 
     response = client.models.generate_content(
-        model="gemini-1.5-flash",
+        model="gemini-1.5-flash-latest",
         contents=user_message
     )
 
